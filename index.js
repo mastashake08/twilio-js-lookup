fe9808e2f8ca2cd8c6b29bff70d4ffd5cba8016d
Jyrone Parker <jyrone.parker@gmail.com>
2024-04-24 23:26:06 +0000

    doc

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ env.config();
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
+// https://www.twilio.com/en-us/user-authentication-identity/lookup
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
